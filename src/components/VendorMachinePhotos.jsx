76876c0e3101ed2127048d419d65dc96b4de529f
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-22 12:20:36 +0300

    initial commit

---
+++ src/components/VendorMachinePhotos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import machineImage from '../assets/machineImage.jpeg'
+export default function VendorMachinePhotos() {
+  return (
+    <div>
+      <img src={machineImage} alt="" />
+    </div>
+  )
+}
