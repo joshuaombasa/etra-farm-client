850629e6f9f9ecc6598f1ceb205409a4b7db66e2
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-22 12:20:44 +0300

    initial commit

---
+++ src/components/VendorMachineDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function VendorMachineDetails() {
+  return (
+    <div>
+      <p><strong>Name: </strong>Modest Explorer</p>
+      <p><strong>Category: </strong>Simple</p>
+      <p><strong>Description: </strong> Great Plains has built a reputation for manufacturing durable plows,
+          harrows, and cultivators that can withstand even the most demanding
+          conditions. Their equipment is known for its effectiveness in seedbed
+          preparation and soil management, ensuring a strong foundation for your
+          crops.</p>
+    </div>
+  )
+}
