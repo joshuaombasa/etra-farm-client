03b09ee341a1c2532f29dc2c1c635448b56a7f69
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-22 12:20:07 +0300

    initial commit

---
+++ src/pages/vendor/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+export default function Dashboard() {
+  return (
+    <div className="">
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
