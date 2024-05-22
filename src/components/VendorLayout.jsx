926e54a04e775941f721f238066173324cd680b0
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-22 12:21:01 +0300

    initial commit

---
+++ src/components/VendorLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+export default function VendorLayout() {
+  return (
+    <div className="vendor-layout page-height">
+      <nav className="vendor-layout-nav">
+        <Link to=".">Dashboard</Link>
+        <Link to="income">Income</Link>
+        <Link to="equipment">Equipment</Link>
+        <Link to="reviews">Reviews</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
